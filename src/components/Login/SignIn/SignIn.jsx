c9090b001793286dd6ab41e137514bf7e97a7eb1
Pritam Mandal <pritampmandal@gmail.com>
2026-02-28 16:21:29 +0530

    done some changes on the login page

--- src/components/Login/SignIn/SignIn.jsx
+++ src/components/Login/SignIn/SignIn.jsx
@@ -19,6 +19,8 @@ function SignIn() {
     const { user, status, error } = useSelector((state) => state.auth)
     const currentMode = useSelector((state) => state.mode.currentMode)
 
+    console.log(currentMode)
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
@@ -51,52 +53,48 @@ function SignIn() {
         <>
 
             <div className={`signUpBody h-screen w-full flex justify-center items-center mt-5 text-red 
-                ${currentMode == 'dark' ? 'bg-[#0F1214] text-white' : 'bg-[#dadada] text-black'}`}>
-                <div className={`xl:h-150 xl:w-280 w-[90vw] md:w-[60vw] h-auto py-10  xl:py-0 rounded-2xl flex
-                   ${currentMode == 'dark' ? 'bg-[#ebebeb] text-white' : 'bg-white text-black border-1'} `}>
+                ${currentMode == "dark" ? 'bg-[#0F1214] text-white' : 'bg-[#dadada] text-black'}`}>
 
-                    <div className='w-1/2 h-full bg-gray-800 rounded-l-2xl rounded-r-[40px] hidden xl:flex flex-col items-center justify-center gap-20 '>
-                        <div className='text-center'>
-                            <h1 className='text-white text-4xl'>Welcome Again to MetaMart !</h1> <br />
-                            <p className='text-3xl text-white'>Please login to your account</p>
+                {/* <div className='w-[80vw] mx-auto xl:w-1/2 md:py-15 lg:py-25 h-full rounded-2xl flex items-center justify-center'  > */}
+                <div className={`w-full max-w-sm p-6 rounded-lg border-2  shadow-2xl
+                             ${currentMode == 'dark' ? 'border-[#2a2a2a]' : 'border-gray-200'}`}>
+                
+                    <h2 className="text-5xl font-semibold text-center">Sign In</h2>
+
+                    <form className="mt-4" onSubmit={handleSubmit}>
+                        <div>
+                            <label className={`block text-sm font-medium  ${currentMode == "dark" ? 'text-gray-500' : 'text-gray-900'} `}>Email</label>
+                            <input type="email" className={`w-full px-4 py-2 mt-1 border rounded-lg
+                                    ${currentMode == "dark" ? 'placeholder-gray-500' : 'placeholder-gray-600'}
+                                    `}
+                                placeholder="Enter your email"
+                                value={userEmail} required
+                                onChange={(e) => setUserEmail(e.target.value)} />
                         </div>
-                        <img src="login.svg" alt="" className='w-70' />
-                    </div>
 
-                    <div className='w-[80vw] mx-auto xl:w-1/2 md:py-15 lg:py-25 h-full rounded-2xl flex items-center justify-center text-black'  >
-                        <div className="w-full max-w-sm p-6 rounded-lg ">
-                            <h2 className="text-5xl font-semibold text-center text-gray-900">Sign In</h2>
-
-                            <form className="mt-4" onSubmit={handleSubmit}>
-                                <div>
-                                    <label className="block text-sm font-medium text-gray-600">Email</label>
-                                    <input type="email" className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email"
-                                        value={userEmail} required
-                                        onChange={(e) => setUserEmail(e.target.value)} />
-                                </div>
-
-                                <div className="mt-4">
-                                    <label className="block text-sm font-medium text-gray-600">Password</label>
-                                    <input type="password" className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your password"
-                                        value={userPassword} required
-                                        onChange={(e) => setUserPassword(e.target.value)} />
-                                </div>
-
-                                <button className="w-full px-4 py-2 mt-4 cursor-pointer font-semibold text-white bg-blue-600 rounded-lg hover:bg-blue-600" type="submit" >Login</button>
-                            </form>
-
-                            <div className="mt-4 text-center text-gray-500">OR</div>
-
-                            <div className="mt-4">
-                                <p className='text-[19px] mt-3 text-center'>Create an Account ?
-                                    <NavLink to='/signup' className='text-blue-600'> SignUp</NavLink>
-                                </p>
-                            </div>
+                        <div className="mt-4">
+                            <label className={`block text-sm font-medium  ${currentMode == "dark" ? 'text-gray-500' : 'text-gray-900'} `}>Password</label>
+                            <input type="password" className={`w-full px-4 py-2 mt-1 border rounded-lg 
+                                      ${currentMode == "dark" ? 'placeholder-gray-500' : 'placeholder-gray-600'}`}
+                                placeholder="Enter your password"
+                                value={userPassword} required
+                                onChange={(e) => setUserPassword(e.target.value)} />
                         </div>
-                    </div>
 
+                        <button className="w-full px-4 py-2 mt-4 cursor-pointer font-semibold text-white bg-blue-600 rounded-lg hover:bg-blue-600" type="submit" >Login</button>
+                    </form>
+
+                    <div className="mt-4 text-center text-gray-500">OR</div>
+
+                    <div className="mt-4">
+                        <p className='text-[19px] mt-3 text-center'>Create an Account ?
+                            <NavLink to='/signup' className='text-blue-600'> SignUp</NavLink>
+                        </p>
+                    </div>
                 </div>
             </div>
+
+            {/* </div> */}
 
             <div className=' top-0 left-0 mt-10 z-100 fixed'>
                 {
--- src/components/Login/SignUp/SignUp.jsx
+++ src/components/Login/SignUp/SignUp.jsx
@@ -56,55 +56,50 @@ function SignUp() {
             <div className={`signUpBody h-screen w-full flex justify-center items-center mt-5 text-red 
                 ${currentMode == 'dark' ? 'bg-[#0F1214] text-white' : 'bg-[#dadada] text-black'}`}>
 
-                <div className={`xl:h-150 xl:w-280 w-[90vw] md:w-[60vw] h-auto py-10  xl:py-0 rounded-2xl flex
-                   ${currentMode == 'dark' ? 'bg-[#ebebeb] text-white' : 'bg-white text-black border-1'} `}>
 
-                    <div className='w-1/2 h-full bg-gray-800 rounded-l-2xl rounded-r-[40px] hidden xl:flex flex-col items-center justify-center gap-20 '>
-                        <div className='text-center'>
-                            <h1 className='text-white text-5xl'>Welcome to MetaMart !</h1> <br />
-                            <p className='text-3xl text-white'>Please Create an account</p>
+                {/* <div className='w-[80vw] mx-auto xl:w-1/2 md:py-15 lg:py-25 h-full rounded-2xl flex items-center justify-center' > */}
+                <div className={`w-full max-w-sm p-10 rounded-2xl border-2 shadow-2xl
+                             ${currentMode == 'dark' ? 'border-[#2a2a2a]' : 'border-gray-200'} `}
+                >
+                    <h2 className="text-3xl lg:text-4xl font-semibold text-center ">Create an Account</h2>
+
+                    <form className="mt-4" onSubmit={handleSubmit}>
+                        <div>
+                            <label className={`block text-sm font-medium  ${currentMode == "dark" ? 'text-gray-500' : 'text-gray-900'} `}>Email</label>
+                            <input type="email" className={`w-full px-4 py-2 mt-1 border rounded-lg
+                                      ${currentMode == "dark" ? 'placeholder-gray-500' : 'placeholder-gray-600'}`}
+                                placeholder="Enter your email"
+                                value={userEmail} required
+                                onChange={(e) => setUserEmail(e.target.value)} />
                         </div>
-                        <img src="login.svg" alt="" className='w-70' />
-                    </div>
 
-                    <div className='w-[80vw] mx-auto xl:w-1/2 md:py-15 lg:py-25 h-full rounded-2xl flex items-center justify-center text-black' >
-                        <div className="w-full max-w-sm p-6 rounded-lg ">
-                            <h2 className="text-3xl lg:text-4xl font-semibold text-center text-gray-800">Create an Account</h2>
-
-                            <form className="mt-4" onSubmit={handleSubmit}>
-                                <div>
-                                    <label className="block text-sm font-medium text-gray-600">Email</label>
-                                    <input type="email" className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email"
-                                        value={userEmail} required
-                                        onChange={(e) => setUserEmail(e.target.value)} />
-                                </div>
-
-                                <div className="mt-4">
-                                    <label className="block text-sm font-medium text-gray-600">Password</label>
-                                    <input type="password" className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your password"
-                                        value={userPassword} required
-                                        onChange={(e) => setUserPassword(e.target.value)} />
-                                </div>
-
-                                <button className="w-full px-4 py-2 mt-4 font-semibold text-white hover:bg-gray-800 rounded-lg bg-blue-600 cursor-pointer" type="submit" >Create Account</button>
-                            </form>
-
-                            <div className="mt-4 text-center text-gray-500">OR</div>
-
-                            <div className="mt-4 text-center">
-                                <button className="flex items-center justify-center gap-5 w-full px-4 py-2 font-semibold text-black bg-gray-100 border-2 rounded-lg hover:bg-gray-300 cursor-pointer"
-                                    onClick={handleWithgoogle}>
-                                    <img src="google.png" alt="" className='w-5 ' />
-                                    Continue with Google
-                                </button>
-                                <p className='text-[19px] mt-3'>already have an account ?
-                                    <NavLink to='/signin' className='text-blue-600'> SignIn</NavLink>
-                                </p>
-                            </div>
+                        <div className="mt-4">
+                            <label className={`block text-sm font-medium  ${currentMode == "dark" ? 'text-gray-500' : 'text-gray-900'} `}>Password</label>
+                            <input type="password" className={`w-full px-4 py-2 mt-1 border rounded-lg
+                                      ${currentMode == "dark" ? 'placeholder-gray-500' : 'placeholder-gray-600'}`}
+                                placeholder="Enter your password"
+                                value={userPassword} required
+                                onChange={(e) => setUserPassword(e.target.value)} />
                         </div>
+
+                        <button className="w-full px-4 py-2 mt-4 font-semibold text-white hover:bg-gray-800 rounded-lg bg-blue-600 cursor-pointer" type="submit" >Create Account</button>
+                    </form>
+
+                    <div className="mt-4 text-center text-gray-500">OR</div>
+
+                    <div className="mt-4 text-center">
+                        <button className="flex items-center justify-center gap-5 w-full px-4 py-2 font-semibold text-black bg-gray-100 border-2 rounded-lg hover:bg-gray-300 cursor-pointer"
+                            onClick={handleWithgoogle}>
+                            <img src="google.png" alt="" className='w-5 ' />
+                            Continue with Google
+                        </button>
+                        <p className='text-[19px] mt-3'>already have an account ?
+                            <NavLink to='/signin' className='text-blue-600'> SignIn</NavLink>
+                        </p>
                     </div>
                 </div>
             </div>
+            {/* </div> */}
 
             <div className=' top-0 left-0 mt-10 z-100 fixed'>
                 {
